25e7f6536db35d30d6c1c236a8e8f4e26b84dba9
rexbodoia <rex@bodoia.com>
2018-07-09 16:55:48 -0700

    Complete UsersController

--- RoutesAndControllers/app/controllers/users_controller.rb
+++ RoutesAndControllers/app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(params.require(:user).permit(:name, :email))
+    @user = User.new(user_params)
     if @user.save
       render json: @user
     else
@@ -16,6 +16,31 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     render json: @user
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user
+      @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: 422 
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    if @user
+      @user.delete
+      render json: @user
+    else
+      render json: @user.errors.full_messages, status: 422 
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
 
